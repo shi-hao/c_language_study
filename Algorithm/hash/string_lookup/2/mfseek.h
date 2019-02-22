5e6f0295a83031773f7230a8542b81fd0428ea7b
shi-hao <shi7631470@163.com>
2019-02-22 15:54:10 +0800

    edi

--- Algorithm/hash/string_lookup/2/mfseek.h
+++ Algorithm/hash/string_lookup/2/mfseek.h
@@ -5,14 +5,14 @@
 #define   debug_print    0
 
 #define     MAX_LINES     (128 * 1024 * 1024)    //max line of a file
-#define		MAX_CHAR      1024 //max character number of one line
+#define		MAX_CHAR      2048 //max character number of one line
 
 #define		BUF_LEN       (MAX_CHAR + 2)  //buf len
 
 struct fileInfo{
 	FILE * file; //file descriptor to access the file
 	unsigned int   cur_max_line;       //current max line number
-	unsigned int   lineTotal[MAX_LINES];//total char of every line
+	unsigned int   lineTotal[MAX_LINES];//total char of all previous lines
 };
 
 //struct saving the file infomation
